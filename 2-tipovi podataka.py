da8e2ef287f0f918077f37fca90b6a291e3cf523
Mare008 <mozpong@gmail.com>
2024-03-10 20:09:39 +0100

    tipovi podataka

---
+++ 2-tipovi podataka.py
@@ -0,0 +1,68 @@
+# ### TIPOVI PODATAKA ###
+
+# # integer #int
+
+# a = 3
+# b=200
+# c=200
+# print(a ,b ,c)
+# print(type(a))
+
+# #FLOAT, decimalni brojevi
+
+# pi=3.14
+# x=4.7
+# y=299.00
+# print(pi, x, y)
+# print(type(pi))
+
+# #kopleksni brojevi, complex
+
+# x=2+3j
+# y=1.5-4.8j
+# print(x)
+# print(y)
+# print(type(x))
+
+# #STRING
+
+# a='python je totalno ludilo'
+# print(a)
+# print(type(a))
+
+# #LISTE,setovi, nizovi(promjenjivi tip podataka)
+
+# liste=[100, 'python', 3.14, True, False,['1', '2', '3']]
+# print(liste)
+# print(type(liste))
+
+# #BOOLEANS(logickin tipovi)
+
+# print(True)
+# print(False)
+# print(type(True))
+
+#RJEČNICI, dictionary, asocijativni nizovi, kljuc i vrijednost
+
+# kursevi={'kurs':  'python za pocetnike', 'predavac': 'Slobodan Miric'}
+# #indeksi    [0]             [1]              [2]             [3]
+# print(kursevi)
+# print(type(kursevi))
+
+#TUPLEs, torke: neizmjenjiva lista podataka, uređena indeksima
+
+kurs=('python', 3, 'mjeseca')
+print(kurs[0] +  ' traje ' +  str(kurs[1]) +  ' '  + kurs[2])
+print(type(kurs))
+
+#SET, skupovi, neuređeni kolekcija jedinstvenih elemenata
+
+a= {'a', 'e', 'i', 'o', 'u', 'a'}
+print(a)
+print(type(a))
+
+
+
+
+
+
