9565e0f7ef1085797587feaa0071fea4e7705480
Mare008 <mozpong@gmail.com>
2024-03-11 20:52:22 +0100

    primjenjena matematika zadatak2

---
+++ 3-vjezbe prim. mat.py
@@ -0,0 +1,11 @@
+napon = []
+struja = []
+
+with open('measurments.txt', 'r') as file:
+    for line in file:
+        napon_str, struja_str = line.split()
+        napon.append(float(napon_str))
+        struja.append(float(struja_str))
+
+print("Napon:", napon)
+print("Struja:", struja)
