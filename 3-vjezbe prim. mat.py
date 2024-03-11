171648eb822b31a088d1bd59ba3d880d0a966ad3
Mare008 <156645266+Mare008@users.noreply.github.com>
2024-03-11 20:56:06 +0100

    Update 3-vjezbe prim. mat.py

--- 3-vjezbe prim. mat.py
+++ 3-vjezbe prim. mat.py
@@ -9,3 +9,12 @@ with open('measurments.txt', 'r') as file:
 
 print("Napon:", napon)
 print("Struja:", struja)
+
+# #Kreirajmo datoteku measurements.txt s mjerenjima napona i struje u
+# nekom od uređivača teksta: i save u file od pythona gdje su ti python vjezbe
+#jer inace nece citati file koji pozivas
+# 0.5 10
+# 1.0 20
+# 1.5 30
+# 2.0 40
+# 2.5 50
